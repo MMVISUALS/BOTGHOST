d7f4b1c9e3db2973b376495c81c92163a2a481a2
MMVISUALS <marimarbequio9@gmail.com>
2025-02-03 01:44:09 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to BINI", "🎧 Listening to BIN"];
+const statusMessages = ["🎧 Listening to BINI", "🎧 Listening to BINI"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
